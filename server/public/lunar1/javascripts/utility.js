dfcb93cbce2e686301ee98024976f0ace26391d4
Calvin <soh_calvin@yahoo.com>
2015-07-12 22:15:38 +0800

    added lookup2.html and utility.js

---
+++ server/public/lunar1/javascripts/utility.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+var SvgUtil = function() {
+
+	var addPattern  = function (svg, patternName, picUrl, width, height  ){
+		var defs = svg.append('svg:defs');
+		defs.append("svg:pattern")
+		    .attr("id", patternName)
+		    .attr("width", width)
+		    .attr("height", height)
+		    .attr("patternUnits", "userSpaceOnUse")
+		    //.attr("patternUnits", "objectBoundingBox") // this is the default
+		    .append("svg:image")
+		    .attr("xlink:href", picUrl)
+		    .attr("width", width)
+		    .attr("height", height)
+		    .attr("x", 0)
+		    .attr("y", 0);
+	};
+	
+	return {
+		"addPattern" : addPattern
+	};
+}();
